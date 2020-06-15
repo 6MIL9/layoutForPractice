c7514e91fff0e703b7f4cf6538c1ba8a937e7745
6MIL9 <emilkliavlin2@gmail.com>
2020-06-15 23:59:32 +0500

    add scroll observer

--- src/components/Spotlights/SpotlightOne.jsx
+++ src/components/Spotlights/SpotlightOne.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import classes from './SpotlightOne.module.scss';
 
@@ -7,11 +7,52 @@ const Wrapper = styled.div`
     background-color: #51CCA8;
 `;
 
+const Inner = styled.div`
+    height: 100%;
+    margin: 0 auto;
+    background-color: #FFFFFF;
+`;
 
 const SpotlightOne = () => {
+
+    const [scroll, setScroll] = React.useState(0);
+
+    const handleScroll = () => {
+        setScroll(window.scrollY);
+    };
+
+    let Flag = null;
+
+    let ViewportH = window.innerHeight;
+
+
+    React.useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+
+    // const Px = () => {
+    //     console.log(scroll);
+    //     console.warn(ViewportH);
+    //     if ((scroll * 4) >= ViewportH) {
+    //         console.warn(true);
+    //         Flag=true;
+    //         window.removeEventListener("scroll", handleScroll);
+    //     }
+    // }
+
+    // Px();
+
+    // if ((scroll * 4) >= ViewportH) {
+    //     Flag = true;
+    // }
+
     return (
         <Wrapper>
             <div className={classes.img}></div>
+            {/* {Flag && <div>sss</div>} */}
+            <Inner/>
         </Wrapper>
     )
 }
