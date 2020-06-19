4f9b7bdbc154071c9a6648b779e008c9a8ff6fec
6MIL9 <emilkliavlin2@gmail.com>
2020-06-19 17:18:01 +0300

    Updates

---
+++ precache-manifest.f4d03879e1656f286e93bd80434b99e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c94aaaf86c32f18b95b4be0226291b3",
+    "url": "/6MIL9/layoutForPractice/index.html"
+  },
+  {
+    "revision": "f2589c2cfb12b2179cb3",
+    "url": "/6MIL9/layoutForPractice/static/css/main.a42da264.chunk.css"
+  },
+  {
+    "revision": "aea136d228372f5b3207",
+    "url": "/6MIL9/layoutForPractice/static/js/2.d1008941.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/6MIL9/layoutForPractice/static/js/2.d1008941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2589c2cfb12b2179cb3",
+    "url": "/6MIL9/layoutForPractice/static/js/main.9665bb87.chunk.js"
+  },
+  {
+    "revision": "4d212b6c0d21ca7a074a",
+    "url": "/6MIL9/layoutForPractice/static/js/runtime-main.18c19079.js"
+  },
+  {
+    "revision": "64716e7efb021724cc284db768230c61",
+    "url": "/6MIL9/layoutForPractice/static/media/town.64716e7e.jpg"
+  }
+]);
